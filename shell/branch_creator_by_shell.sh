b27242f90080605882c9633d7695bdb152a1c69b
koba-masa <kobayashi_mshr@koba-masa.com>
2022-02-13 18:42:50 +0900

    fix: 直した

--- shell/branch_creator_by_shell.sh
+++ shell/branch_creator_by_shell.sh
@@ -4,7 +4,7 @@ REPOSITORY_CONFIG="shell/config/repositories.conf"
 
 BASE_BRANCH="develop"
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
   echo "[ERROR] A mount of paramater is wrong."
   exit
 fi
@@ -12,7 +12,37 @@ fi
 new_branch_name=$1
 repositories=`cat ${REPOSITORY_CONFIG}`
 work_dir=`pwd`
-commit_message="hoge"
+release_date=$2
+
+if [[ "${new_branch_name}" =~ ^release/.*_emergency$ ]]; then
+  release_type_name="緊急"
+elif [[ "${new_branch_name}" =~ ^release/monthly_update_[0-9]{6}.*$ ]]; then
+  release_type_name="月次"
+elif [[ "${new_branch_name}" =~ ^release/[0-9]{8}$ ]]; then
+  release_type_name="通常"
+else
+  echo "[ERROR] Branch name is wrong.:${new_branch_name}"
+  exit
+fi
+
+commit_message="${release_date} ${release_type_name}リリース"
+
+echo "[INFO ] チェック"
+echo "  branch_name      : ${new_branch_name}"
+echo "  release_type_name: ${release_type_name}"
+echo "  release_date     : ${release_date}"
+echo "  branches         : "
+for repository in ${repositories}
+do
+  echo "    ${repository}"
+done
+
+echo "Is it OK?[Y/n]:"
+read answer
+if [ "${answer}" != "Y" ]; then
+  echo "[INFO ] Process is abort."
+  exit
+fi
 
 for repository in ${repositories}
 do
